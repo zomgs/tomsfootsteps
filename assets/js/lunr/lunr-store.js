1aba1bfe6d8d3c952de906bfb200582be2c7dc9f
zomgs <normal.mma@gmail.com>
2019-04-03 11:35:36 +0200

    second commit

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Post: Modified Date",
+        "excerpt":"Check for long titles and how they might break a template.  ","categories": ["blog"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/edge-case-very-long-title/",
+        "teaser":"http://localhost:4000/assets/images/bg.jpg"},{
+        "title": "Solo Trekking Baliem Valley, Papua, Indonesia",
+        "excerpt":"Trekking in Baliem Valley is challenging without proper gear. A few months prior, Papua hadn’t been on my radar. The few things I had heard about the place had mostly discouraged me from visiting. While on Borneo, one of the other Indonesian islands, this view changed as my curiosity was...","categories": ["blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "http://localhost:4000/blog/solo-trekking-baliem-valley/",
+        "teaser":null}]
